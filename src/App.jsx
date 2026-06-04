14c1a15c9f7f3369b9f1ec49727a564616df539d
Afsal Prima Maula <afsalprimamaulaaa@gmail.com>
2026-06-04 14:00:58 +0700

    Perbaikan: Memisahkan komponen TopBar dan Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
-import TopBar from './components/TopBar';
+import TopBar from './components/TopBar'; // Import TopBar
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      {/* Memanggil komponen Header */}
+      
+      {/* Panggil komponen TopBar di sini agar posisinya paling atas */}
+      <TopBar />
+      
+      {/* Panggil komponen Header di bawahnya */}
       <Header />
       
       {/* Area untuk Hero Section selanjutnya */}
       <main className="flex-grow flex items-center justify-center">
         <p className="text-gray-500">Area Hero Section akan dibuat di sini...</p>
       </main>
+      
     </div>
   );
 }
