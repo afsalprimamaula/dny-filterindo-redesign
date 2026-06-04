0c84c1d132f628794ad5f0d8b2dd2abadb039696
Afsal Prima Maula <afsalprimamaulaaa@gmail.com>
2026-06-04 14:43:04 +0700

    featuressection fiks

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import ClientsSection from './components/ClientsSection';
-import FeaturesSection from './components/FeaturesSection';
+import FeaturesSection from './components/FeaturesSection'; // Pastikan import ini ada
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <main className="flex-grow">
         <HeroSection />
         <ClientsSection />
+        <FeaturesSection /> {/* Pastikan tag ini ada di dalam main */}
       </main>
       
     </div>
